6fe6e953b90dcebc5b09e4da85727f69ee151cf4
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-27 19:39:46 -0300

    Criado o API mercearia João

---
+++ src/main/java/com/merceariajoao/cadastro/MerceariaApplication.java
@@ -0,0 +1,13 @@
+package com.merceariajoao.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerceariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerceariaApplication.class, args);
+	}
+
+}
