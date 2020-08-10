5e719d8ebd1ca7f018672abaa555335ecfa013d1
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-08-10 19:24:00 -0300

    Refatoração do codico da classe model utilizado Lombok

--- src/main/java/com/merceariajoao/cadastro/model/Produtos.java
+++ src/main/java/com/merceariajoao/cadastro/model/Produtos.java
@@ -7,14 +7,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "produtos")
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Produtos {
 
 	
 	@Id
 	@Column(name = "codigo")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private long codigo;
 	
 	@Column(name = "nome_produto")
@@ -30,91 +37,6 @@ public class Produtos {
 	private String lote;
 	
 
-	public long getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(long codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
-
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
-
-	public String getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-
-	public String getDataValidade() {
-		return dataValidade;
-	}
-
-	public void setDataValidade(String dataValidade) {
-		this.dataValidade = dataValidade;
-	}
-
-	public String getLote() {
-		return lote;
-	}
-
-	public void setLote(String lote) {
-		this.lote = lote;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
-		result = prime * result + (int) (codigo ^ (codigo >>> 32));
-		result = prime * result + ((dataValidade == null) ? 0 : dataValidade.hashCode());
-		result = prime * result + ((lote == null) ? 0 : lote.hashCode());
-		result = prime * result + ((nomeProduto == null) ? 0 : nomeProduto.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produtos other = (Produtos) obj;
-		if (categoria == null) {
-			if (other.categoria != null)
-				return false;
-		} else if (!categoria.equals(other.categoria))
-			return false;
-		if (codigo != other.codigo)
-			return false;
-		if (dataValidade == null) {
-			if (other.dataValidade != null)
-				return false;
-		} else if (!dataValidade.equals(other.dataValidade))
-			return false;
-		if (lote == null) {
-			if (other.lote != null)
-				return false;
-		} else if (!lote.equals(other.lote))
-			return false;
-		if (nomeProduto == null) {
-			if (other.nomeProduto != null)
-				return false;
-		} else if (!nomeProduto.equals(other.nomeProduto))
-			return false;
-		return true;
-	}
 
 		
 
